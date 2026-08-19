b6a6f0e6585d2a27518834bec6202b6597b98fd0
fabien.roche <sebrosa25@gmail.com>
2026-08-19 10:51:53 +0200

    Correction affichage automatique version application

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.saramakadicto"
         minSdk = 24
         targetSdk = 35
-        versionCode = 344
-        versionName = "3.7.0-beta1"
+        versionCode = 345
+        versionName = "3.8.0-beta1"
     }
 
     compileOptions {
