6ef01974e2f2182ec71219a02224b252e0bcb346
fabien roche <sebrosa25@gmail.com>
2026-08-25 17:25:13 +0200

    chore(release): prepare 3.9.0-beta2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.saramakadicto"
         minSdk = 24
         targetSdk = 35
-        versionCode = 346
-        versionName = "3.9.0-beta1"
+        versionCode = 347
+        versionName = "3.9.0-beta2"
     }
 
     compileOptions {
