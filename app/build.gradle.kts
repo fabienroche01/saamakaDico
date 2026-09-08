de5a97b3d1372c804efc2bf6508d10a3b0d6d208
fabien roche <sebrosa25@gmail.com>
2026-09-08 11:29:56 +0200

    chore(release): bump 3.9.0 to build 357

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.saramakadicto"
         minSdk = 24
         targetSdk = 36
-        versionCode = 356
+        versionCode = 357
         versionName = "3.9.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
