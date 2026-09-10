309cb3c3098ba61130d6c77607e977143440d954
fabienroche01 <sebrosa25@gmail.com>
2026-09-10 12:11:15 +0200

    test(grammar): cover Saamaka pronoun roles

--- app/src/test/java/com/saamaka/dico/testeurs/SaamakaGrammarEngineTest.kt
+++ app/src/test/java/com/saamaka/dico/testeurs/SaamakaGrammarEngineTest.kt
@@ -13,7 +13,10 @@ class SaamakaGrammarEngineTest {
             candidate("devoir", "da"),
             candidate("aimer", "lobi"),
             candidate("manger", "Makandi", "O"),
-            candidate("dormir", "duumí")
+            candidate("dormir", "duumí"),
+            candidate("voir", "si"),
+            candidate("aider", "heepi"),
+            candidate("mère", "mama")
         )
     )
     private val engine = SaamakaGrammarEngine(resolver::resolve)
@@ -22,6 +25,8 @@ class SaamakaGrammarEngineTest {
     fun appliesAttestedCopulaMarkersWithoutTranslatingEtre() {
         assertEquals("mi síki", engine.translate("je suis malade")?.translation)
         assertEquals("i wanwan", engine.translate("tu es seul")?.translation)
+        assertEquals("a síki", engine.translate("elle est malade")?.translation)
+        assertEquals("a síki", engine.translate("il est malade")?.translation)
         assertEquals("mi bi síki", engine.translate("j’étais malade")?.translation)
         assertEquals("mi an síki", engine.translate("je ne suis pas malade")?.translation)
     }
@@ -35,6 +40,35 @@ class SaamakaGrammarEngineTest {
         assertEquals("mi ta Makandi", engine.translate("je suis en train de manger")?.translation)
         assertEquals("mi ta da duumí", engine.translate("je dois dormir")?.translation)
         assertEquals("mi an lobi duumí", engine.translate("je n’aime pas dormir")?.translation)
+    }
+
+    @Test
+    fun grammarLineDistinguishesThirdPersonSubjectFromObject() {
+        assertEquals("a ta si mi", engine.translate("elle me voit")?.translation)
+        assertEquals("mi ta si ën", engine.translate("je la vois")?.translation)
+        assertEquals("mi ta si ën", engine.translate("je le vois")?.translation)
+        assertEquals("mi ta heepi ën", engine.translate("je l’aide")?.translation)
+        assertEquals("mi an si ën", engine.translate("je ne la vois pas")?.translation)
+    }
+
+    @Test
+    fun grammarLineKeepsWeakObjectPronounsForOtherPersons() {
+        assertEquals("mi ta si i", engine.translate("je te vois")?.translation)
+        assertEquals("mi ta si u", engine.translate("je nous vois")?.translation)
+        assertEquals("mi ta si unu", engine.translate("je vous vois")?.translation)
+        assertEquals("mi ta si de", engine.translate("je les vois")?.translation)
+    }
+
+    @Test
+    fun grammarLineUsesDependentPronounForPossession() {
+        assertEquals("mi ta si i mama", engine.translate("je vois ta mère")?.translation)
+        assertEquals("mi ta si ën mama", engine.translate("je vois sa mère")?.translation)
+    }
+
+    @Test
+    fun grammarLineUsesStrongPronounAfterPour() {
+        assertEquals("mi ta Makandi fu mí", engine.translate("je mange pour moi")?.translation)
+        assertEquals("mi ta Makandi fu hën", engine.translate("je mange pour elle")?.translation)
     }
 
     @Test
